b617624f5daaa26841970d46b267b1f2219ef508
taozhenhua <1248546697@qq.com>
2018-06-13 16:51:23 +0800

    update

--- application/api/service/v1_0_1/Trade.php
+++ application/api/service/v1_0_1/Trade.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\api\service\v1_0_1;
 
+use app\api\model\WithdrawLog as WithdrawLogModel;
 /**
  * 红包服务类
  */
@@ -12,6 +13,16 @@ class Trade
 	 */
 	public static function generateTradeNo()
 	{
-		return 'X1'.date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
+		//return 'Z1'.date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
+
+		$str = 'Z1'.date('ymdHis').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 4);
+
+		$model = WithdrawLogModel::where('trade_no', $str)->find();
+
+		if ($model) {
+			return self::generateTradeNo();
+		}
+
+		return $str;
 	}
 }
