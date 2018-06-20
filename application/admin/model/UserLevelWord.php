e09534ca035702dcbc5fcdb5f517e1e038b2c1bc
taozhenhua <1248546697@qq.com>
2018-06-20 18:06:59 +0800

    update

---
+++ application/admin/model/UserLevelWord.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+/**
+ * 用户等级设置模型类
+ */
+class UserLevelWord extends Model
+{
+	public function search($params)
+	{
+		$query = self::buildQuery();
+
+        foreach (['user_level_id', 'word_level', 'word_num', 'word_time'] as $key) {
+            (isset($params[$key]) && $params[$key] !== '') && $query->where($key, $params[$key]);
+        }
+
+        $query->order('id', 'desc');
+
+        return $query;
+	}
+}
