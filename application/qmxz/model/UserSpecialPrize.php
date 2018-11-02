ad4158e20a864b3b020aaa14bd6f6cb257b4c607
zhourongke <625575737@qq.com>
2018-11-02 11:50:44 +0800

    update

--- application/qmxz/model/UserSpecialPrize.php
+++ application/qmxz/model/UserSpecialPrize.php
@@ -9,4 +9,20 @@ use think\Model;
  */
 class UserSpecialPrize extends Model
 {
+	public function search($params)
+	{
+		$query = self::buildQuery();
+
+		foreach (['user_id'] as $key) {
+            (isset($params[$key]) && $params[$key] !== '') && $query->whereLike($key, "%{$params[$key]}%");
+        }
+
+        foreach (['status'] as $key) {
+            (isset($params[$key]) && $params[$key] !== '') && $query->where($key, $params[$key]);
+        }
+
+        $query->order('id desc');
+
+        return $query;
+	}
 }
