814083d7f453e7a578190b05cee51675d84ee01a
zhourongke <625575737@qq.com>
2018-11-02 08:12:48 +0800

    update

--- application/qmxz/model/Special.php
+++ application/qmxz/model/Special.php
@@ -9,16 +9,21 @@ use think\Model;
  */
 class Special extends Model
 {
-	public function search($params)
-	{
-		$query = self::buildQuery();
+    public function search($params)
+    {
+        $query = self::buildQuery();
 
-		foreach (['title'] as $key) {
+        $start = strtotime(date('Y-m-d 00:00:00'));
+        $end   = strtotime(date('Y-m-d 23:59:59'));
+
+        $query->where('display_time', 'between', [$start, $end]);
+
+        foreach (['title'] as $key) {
             (isset($params[$key]) && $params[$key] !== '') && $query->whereLike($key, "%{$params[$key]}%");
         }
 
         $query->order('id desc');
 
         return $query;
-	}
+    }
 }
