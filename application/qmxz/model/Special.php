9fe16ade6790530c6186326475f01469ae7bc9fe
zhourongke <625575737@qq.com>
2018-11-05 14:11:53 +0800

    update

--- application/qmxz/model/Special.php
+++ application/qmxz/model/Special.php
@@ -13,13 +13,17 @@ class Special extends Model
     {
         $query = self::buildQuery();
 
-        $start = strtotime(date('Y-m-d 00:00:00'));
-        $end   = strtotime(date('Y-m-d 23:59:59'));
-
-        $query->where('display_time', 'between', [$start, $end]);
-
         foreach (['title'] as $key) {
             (isset($params[$key]) && $params[$key] !== '') && $query->whereLike($key, "%{$params[$key]}%");
+        }
+
+        if (isset($params['create_time']) && $params['create_time'] !== '') {
+            list($start_create_time, $end_create_time) = explode(' - ', $params['create_time']);
+            $query->whereBetweenTime('display_time', "{$start_create_time}", "{$end_create_time}");
+        }else{
+            $start = strtotime(date('Y-m-d 00:00:00'));
+            $end   = strtotime(date('Y-m-d 23:59:59'));
+            $query->where('display_time', 'between', [$start, $end]);
         }
 
         $query->order('id desc');
