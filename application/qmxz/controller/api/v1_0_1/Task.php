8db8c1a18805b10df09b7871c6a323eec6c0c14f
zhourongke <625575737@qq.com>
2018-11-06 10:15:12 +0800

    任务中心接口增加用户金币gold字段

--- application/qmxz/controller/api/v1_0_1/Task.php
+++ application/qmxz/controller/api/v1_0_1/Task.php
@@ -8,40 +8,43 @@
 
 namespace app\qmxz\controller\api\v1_0_1;
 
-use think\facade\Request;
-use controller\BasicController;
+use app\qmxz\model\InviteUser;
 use app\qmxz\model\Task as TaskModel;
 use app\qmxz\model\UserRecord as UserRecord;
-use think\Db;
 use app\qmxz\model\UserRegister;
-use app\qmxz\model\InviteUser;
+use controller\BasicController;
+use think\Db;
+use think\facade\Request;
 
 /**
  * Description of Task
  *
  * @author 157900869@qq.com
  */
-class Task extends BasicController {
+class Task extends BasicController
+{
 
     //任务邀请好友数量
     protected $task_invite_number = 2;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
-        $config_data = $this->configData;
-        $this->task_invite_number=$config_data['task_invite_number'];
+        $config_data              = $this->configData;
+        $this->task_invite_number = $config_data['task_invite_number'];
     }
 
-    public function center() {
-        $model = new TaskModel();
-        $openid = Request::param('openid');
-        $list = $model->getTaskList();
-        $day = date("Ymd");
-        $issend = Db::name("task_log")->where(['openid' => $openid, 'day' => $day])->column('type_id,openid,gold,day');
-        $usermodel = new UserRecord();
-        $userinfo = $usermodel->get(['openid' => $openid]);
-        $user_id = $userinfo['user_id'];
-        $regmodel = new UserRegister();
+    public function center()
+    {
+        $model       = new TaskModel();
+        $openid      = Request::param('openid');
+        $list        = $model->getTaskList();
+        $day         = date("Ymd");
+        $issend      = Db::name("task_log")->where(['openid' => $openid, 'day' => $day])->column('type_id,openid,gold,day');
+        $usermodel   = new UserRecord();
+        $userinfo    = $usermodel->get(['openid' => $openid]);
+        $user_id     = $userinfo['user_id'];
+        $regmodel    = new UserRegister();
         $invitemodel = new InviteUser();
         foreach ($list as $key => $val) {
             //判断有没有领取过
@@ -52,109 +55,117 @@ class Task extends BasicController {
             }
             //判断是否签到
             if ($val['id'] == 1) {
-                $is_reg = $regmodel->get(['user_id' => $user_id, 'add_date' => date('ymd')]);
+                $is_reg                = $regmodel->get(['user_id' => $user_id, 'add_date' => date('ymd')]);
                 $list[$key]['is_give'] = $is_reg ? 1 : 0;
                 //判断1.签到的第几天,2.展示可以得到的金币数
-                $res = $regmodel->count_days($user_id);
-                $list[$key]['gold'] = $res['gold'];
+                $res                      = $regmodel->count_days($user_id);
+                $list[$key]['gold']       = $res['gold'];
                 $list[$key]['count_days'] = $res['count_days'];
             }
             //判断今日邀请好友数量
             if ($val['id'] == 3) {
-                $invite_number = $invitemodel->where(['openid' => $openid])->where('create_time' ,'between', [date('Y-m-d') . ' 00:00:00', date('Y-m-d') . ' 23:59:59'])->count();
+                $invite_number                    = $invitemodel->where(['openid' => $openid])->where('create_time', 'between', [date('Y-m-d') . ' 00:00:00', date('Y-m-d') . ' 23:59:59'])->count();
                 $list[$key]['task_invite_number'] = $this->task_invite_number;
-                $list[$key]['invite_number'] = $invite_number;
+                $list[$key]['invite_number']      = $invite_number;
             }
             //普通场押宝
             if ($val['id'] == 4) {
-               
-               $is_play_topic = Db::name('user_topic')->where(['user_id' => $user_id, 'create_date' => date('ymd')])->find();
-               $list[$key]['is_give'] = $is_play_topic ? 1 : 0;
+
+                $is_play_topic         = Db::name('user_topic')->where(['user_id' => $user_id, 'create_date' => date('ymd')])->find();
+                $list[$key]['is_give'] = $is_play_topic ? 1 : 0;
             }
             //普通场押宝
             if ($val['id'] == 5) {
-               $is_play_special = Db::name('user_special')->where(['user_id' => $user_id, 'create_date' => date('ymd')])->find();
-               $list[$key]['is_give'] = $is_play_special ? 1 : 0;
+                $is_play_special       = Db::name('user_special')->where(['user_id' => $user_id, 'create_date' => date('ymd')])->find();
+                $list[$key]['is_give'] = $is_play_special ? 1 : 0;
             }
 
             //反悔卡
             if ($val['id'] == 6) {
                 //反悔卡每日获取数量与邀请好友数量一致
-               $list[$key]['regret_times'] = Db::name("regret_card")->where(['openid' => $openid])->where('add_date',date('ymd'))->value('times');
-               $list[$key]['task_regret_times'] = $this->task_invite_number;
+                $list[$key]['regret_times']      = Db::name("regret_card")->where(['openid' => $openid])->where('add_date', date('ymd'))->value('times');
+                $list[$key]['task_regret_times'] = $this->task_invite_number;
             }
-         
+
         }
 
         //签到列表
         $sign_list = Db::name('user_register_config')->select();
 
+        //用户当前金币
+        $gold = Db::name('user_record')->where(['openid' => $openid])->field('gold')->find()['gold'];
+
         $res = [
+            'gold'      => $gold,
             'sign_list' => $sign_list,
-            'list' => $list
+            'list'      => $list,
         ];
 
         return result('200', "ok", $res);
     }
 
     //记录分享奖励
-    public function share() {
+    public function share()
+    {
         $openid = Request::param('openid');
-        $model = new TaskModel();
-        $data = $model->sendTask($openid, 2);
+        $model  = new TaskModel();
+        $data   = $model->sendTask($openid, 2);
         return result('200', "ok", $data);
     }
-    
+
     //普通场押宝
-    public function game_one() {
-        $request = Request::param();
-        $data = [];
+    public function game_one()
+    {
+        $request       = Request::param();
+        $data          = [];
         $is_play_topic = Db::name('user_topic')->where(['user_id' => $request['user_id'], 'create_date' => date('ymd')])->find();
-        if($is_play_topic){
+        if ($is_play_topic) {
             $model = new TaskModel();
-            $data = $model->sendTask($request['openid'], 4);
+            $data  = $model->sendTask($request['openid'], 4);
         }
         return result('200', "ok", $data);
     }
 
     //整点挑战赛
-    public function game_top() {
-        $request = Request::param();
-        $data = [];
+    public function game_top()
+    {
+        $request         = Request::param();
+        $data            = [];
         $is_play_special = Db::name('user_special')->where(['user_id' => $request['user_id'], 'create_date' => date('ymd')])->find();
-        if($is_play_special){
+        if ($is_play_special) {
             $model = new TaskModel();
-            $data = $model->sendTask($request['openid'], 5);
+            $data  = $model->sendTask($request['openid'], 5);
         }
         return result('200', "ok", $data);
     }
-    
+
     //签到
-    public function register() {
-        $model = new UserRegister();
+    public function register()
+    {
+        $model  = new UserRegister();
         $openid = Request::param('openid');
-        $data = $model->remark($openid);
-       return result('200', "ok", $data);
+        $data   = $model->remark($openid);
+        return result('200', "ok", $data);
     }
 
     //记录邀请
-    public function invite_user() {
+    public function invite_user()
+    {
 
         $task = new TaskModel();
         //点击人openid
         $from_user = Request::param('from_openid');
         //分享人openid
-        $openid = Request::param('openid');
-        $model = new InviteUser();
-        $data = $model->remark($from_user, $openid);
+        $openid        = Request::param('openid');
+        $model         = new InviteUser();
+        $data          = $model->remark($from_user, $openid);
         $invite_number = $model->getInviteDayCount($openid, date('Y-m-d'));
-        if ($invite_number == $this->task_invite_number && $data['code']==0) {
+        if ($invite_number == $this->task_invite_number && $data['code'] == 0) {
             $task->sendTask($openid, 3);
             //增加完成反悔卡的任务日志
             $task->sendTask($openid, 6);
         }
-       return result('200', "ok", $data);
+        return result('200', "ok", $data);
     }
-
 
 }
