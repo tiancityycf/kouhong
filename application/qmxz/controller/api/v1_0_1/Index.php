b38b2bbc5852f250d89923116cfaa7506e7251f6
zhourongke <625575737@qq.com>
2018-11-10 14:56:12 +0800

    update

--- application/qmxz/controller/api/v1_0_1/Index.php
+++ application/qmxz/controller/api/v1_0_1/Index.php
@@ -4,8 +4,8 @@ namespace app\qmxz\controller\api\v1_0_1;
 
 use app\qmxz\service\v1_0_1\Index as IndexService;
 use controller\BasicController;
-use think\facade\Request;
 use think\Db;
+use think\facade\Request;
 
 /**
  * 首页接口控制器类
@@ -36,15 +36,15 @@ class Index extends BasicController
     {
         //echo "<pre>"; print_r($this->configData);exit();
         require_params('user_id');
-        $user_id = Request::param('user_id');
-        $result['user_info'] = Db::name('user_record')->field('avatar,nickname,gold,money')->where('user_id',$user_id)->find();
-        $rules = $this->configData['rules'];
+        $user_id             = Request::param('user_id');
+        $result['user_info'] = Db::name('user_record')->field('avatar,nickname,gold,money')->where('user_id', $user_id)->find();
+        $rules               = $this->configData['rules'];
 
         foreach ($rules as $key => $value) {
-            list($result['rules'][$key]['title'],$contents) = explode('|', $value); 
-            if(strpos($contents,'&')){
-                 $result['rules'][$key]['content'] = explode('&',$contents); 
-            }else{
+            list($result['rules'][$key]['title'], $contents) = explode('|', $value);
+            if (strpos($contents, '&')) {
+                $result['rules'][$key]['content'] = explode('&', $contents);
+            } else {
 
                 $result['rules'][$key]['content'][] = $contents;
             };
@@ -53,17 +53,16 @@ class Index extends BasicController
         $common_problems = $this->configData['common_problems'];
 
         foreach ($common_problems as $k2 => $v2) {
-            list($result['common_problems'][$k2]['title'],$problems) = explode('|', $v2); 
-            if(strpos($problems,'&')){
-                 $result['common_problems'][$k2]['content'] = explode('&',$problems);
-                 $result['common_problems'][$k2]['id'] =  $k2+1;
-            }else{
+            list($result['common_problems'][$k2]['title'], $problems) = explode('|', $v2);
+            if (strpos($problems, '&')) {
+                $result['common_problems'][$k2]['content'] = explode('&', $problems);
+                $result['common_problems'][$k2]['id']      = $k2 + 1;
+            } else {
 
                 $result['common_problems'][$k2]['content'][] = $problems;
-                $result['common_problems'][$k2]['id'] =  $k2+1;
+                $result['common_problems'][$k2]['id']        = $k2 + 1;
             };
         }
-
 
         $result['common_problems_banner'] = $this->configData['common_promble_banner'];
 
@@ -74,10 +73,25 @@ class Index extends BasicController
      * 玩家金币排行接口
      * @return boolean
      */
-     public function rank()
+    public function rank()
     {
-        $rank_nums = $this->configData['rank_nums'] ? $this->configData['rank_nums'] : 5;
-        $rankers = Db::name('user_record')->field('avatar,nickname,gold')->order('gold desc')->limit($rank_nums)->select();
+        $config_data = $this->configData;
+        //默认用户名
+        $system_default_nickname = $config_data['system_default_nickname'];
+        //默认头像
+        $system_default_avatar = $config_data['system_default_avatar'];
+        $rank_nums             = $config_data['rank_nums'] ? $config_data['rank_nums'] : 5;
+        $rankers               = Db::name('user_record')->field('avatar,nickname,gold')->order('gold desc')->limit($rank_nums)->select();
+        if (!empty($rankers)) {
+            foreach ($rankers as $key => $value) {
+                if ($value['avatar'] == '') {
+                    $rankers[$key]['avatar'] = $system_default_avatar;
+                }
+                if ($value['nickname'] == '') {
+                    $rankers[$key]['nickname'] = $system_default_nickname;
+                }
+            }
+        }
         return result(200, 'ok', $rankers);
     }
 
