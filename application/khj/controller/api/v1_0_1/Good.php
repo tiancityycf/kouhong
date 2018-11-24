a6b1d3b5064c354e84d67c2920ccb5715ef13bd0
杨存峰 <490868351@qq.com>
2018-11-24 14:05:28 +0800

    any

--- application/khj/controller/api/v1_0_1/Good.php
+++ application/khj/controller/api/v1_0_1/Good.php
@@ -21,6 +21,9 @@ class Good extends BasicController
         //前台测试链接：https://khj.wqop2018.com/khj/api/v1_0_1/good/index.html;
         //按照价格升序
         $goods_info = Db::name('good_cates')->alias('a')->join(['t_goods' => 'b'], 'a.id=b.cate')->where(['b.status' => 1])->order('b.order asc')->field("a.cate_name,b.*")->select();
+        foreach($goods_info as $k=>$v){
+            $goods_info[$k]['imgs'] = Db::name('good_imgs')->where(['product_id' => $v['id']])->field("img")->select();
+        }
         $data['good_info'] = $goods_info;
 
         $data['rules'] = [];
