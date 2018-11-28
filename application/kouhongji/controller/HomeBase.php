4fbf447356c2fac3d5fc426d57c64c8a7b987707
zhourongke <625575737@qq.com>
2018-11-28 16:12:49 +0800

    update

--- application/kouhongji/controller/HomeBase.php
+++ application/kouhongji/controller/HomeBase.php
@@ -5,15 +5,28 @@ use think\Controller;
 
 class HomeBase extends Controller
 {
-	/**
+    /**
      * 控制器基础方法
      */
     public function initialize()
     {
-    	trace('uid='.session('uid'),'error');
+        trace('uid=' . session('uid'), 'error');
         if (!session('uid')) {
-        	trace('用户uid不存在','error');
-        	$this->redirect("login/index");
+            trace('用户uid不存在', 'error');
+            $this->redirect("login/index");
+        } else {
+            $status      = Request::param('status');
+            $user_id     = Request::param('user_id');
+            $last_login  = Request::param('last_login');
+            $openid      = Request::param('openid');
+            $user_status = Request::param('user_status');
+            $money       = Request::param('money');
+            $this->assign('status', $status);
+            $this->assign('user_id', $user_id);
+            $this->assign('last_login', $last_login);
+            $this->assign('openid', $openid);
+            $this->assign('user_status', $user_status);
+            $this->assign('money', $money);
         }
     }
 }
