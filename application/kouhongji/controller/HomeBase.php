ef941b04aef4025c592881b246d96523d96f186f
zhourongke <625575737@qq.com>
2018-11-29 14:49:46 +0800

    update

--- application/kouhongji/controller/HomeBase.php
+++ application/kouhongji/controller/HomeBase.php
@@ -24,17 +24,11 @@ class HomeBase extends Controller
             $this->assign('money', $money);
         } else {
             if (session('uid')) {
-                trace('用户uid存在', 'error');
                 $user_id     = session('uid');
                 $last_login  = session('last_login');
                 $openid      = session('openid');
                 $user_status = session('user_status');
                 $money       = session('money');
-                trace('uid2=' . $user_id, 'error');
-                trace('last_login1=' . $last_login, 'error');
-                trace('openid1=' . $openid, 'error');
-                trace('user_status1=' . $user_status, 'error');
-                trace('money1=' . $money, 'error');
                 $this->assign('user_id', $user_id);
                 $this->assign('last_login', $last_login);
                 $this->assign('openid', $openid);
