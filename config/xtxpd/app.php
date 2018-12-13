ef632af9e488e9202935548a352c672265614bde
杨存峰 <490868351@qq.com>
2018-12-13 09:08:15 +0800

    小甜心炮弹

---
+++ config/xtxpd/app.php
@@ -0,0 +1,11 @@
+<?php
+//配置文件
+return [
+    'ad_url'              => 'https://ad.ali-yun.wang',
+    'config_key'          => 'config_info',
+    //异常系统接入参数
+    'system_title'        => '小甜心炮弹',
+    'system_sign'         => 'khj.wqop2018.comxtxpd',
+    'system_url'          => 'http://log.zxmn2018.com/logs/api/v1/exceptionlog/writeExceptionlog',
+
+];
