22fc04c75fba85210754bd7e423fe3cb7b41761d
杨存峰 <490868351@qq.com>
2018-12-13 09:25:10 +0800

    any

--- application/xtxpd/controller/api/v1_0_1/Index.php
+++ application/xtxpd/controller/api/v1_0_1/Index.php
@@ -13,7 +13,7 @@ use think\facade\Request;
 class Index extends BasicController
 {
     /**
-     * 拉去首页切换的开关
+     * 拉去首页配置 审核开关等
      * @return json
      */
 
@@ -21,15 +21,10 @@ class Index extends BasicController
     {
         //前台测试链接：https://xtxpd.wqop2018.com/xtxpd/api/v1_0_1/index/index
         $data = Request::param();
-        $switch = 0;
-        $iospay = 0;
         if(isset($data['version'])){
-            $switch = isset($this->configData['switch_'.$data['version']])?$this->configData['switch_'.$data['version']]:0;
-            $iospay = isset($this->configData['ios_pay_'.$data['version']])?$this->configData['ios_pay_'.$data['version']]:0;
+            $check = isset($this->configData['check_'.$data['version']])?$this->configData['check_'.$data['version']]:0;
         }
-        $data['switch'] = $switch ; //1:审核中 0:审核通过
-        //0-开启IOS支付  1-关闭IOS支付
-        $data['isIOS'] = $iospay ;
+        $data['check'] = $check ; //1:审核中 0:审核通过
         return result(200, 'ok', $data);
     }
 }
